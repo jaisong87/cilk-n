4b07592f01d08f555d880dd12a06cc51895a8d80
Jaison George <jaisong87@gmail.com>
2012-02-12 16:48:05 -0600

    modified job script

--- GaussianElimination/job.sh
+++ GaussianElimination/job.sh
@@ -7,10 +7,10 @@
 #$ -N gaussianElemination
 #$ -o output-$JOB_NAME
 #$ -e error-$JOB_NAME
-#$ -M <username>@gmail.com
+#$ -M jaisong87@gmail.com
 #$ -m be
 #$ -l h_rt=01:00:00
 
 export PATH=$PATH:$HOME/cilk/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/cilk/lib
-./gaussianElimination < "SampleIO/ge-in.txt" > output.txt
+/usr/bin/time ./gaussianElimination -cilk_set_worker_count=1 < "ge-in.txt" 2>ge-timeElapsed.txt
