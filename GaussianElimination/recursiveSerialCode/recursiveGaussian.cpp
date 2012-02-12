71e791060f2e23f5d5f1f8e0a110455b06adcdbe
Jaison George <jaisong87@gmail.com>
2012-02-11 20:09:33 -0500

    Serial and recursive code finished

--- GaussianElimination/recursiveSerialCode/recursiveGaussian.cpp
+++ GaussianElimination/recursiveSerialCode/recursiveGaussian.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<cstdio>
 #include<iomanip>
 #include<cassert>
+#include<sstream>
+#include<bitset>
 using namespace std;
 
 double ** Matrix = NULL; // Bad code!! Need to add a class
+bool *** updateMap = NULL;
 int N;
 static int updateCount = 0, updateCall = 0;
 bool dbg = false;//true;
@@ -12,35 +16,74 @@ bool dbg = false;//true;
 void initializeMatrix(int N)
 {
 Matrix = new double*[N+1];
+updateMap = new bool**[N+1];
+
 for(int i=1;i<=N;i++)
+	{
 	Matrix[i] = new double[N+1];
+	updateMap[i] = new bool*[N+1];
+	}
 
 for(int i=1;i<=N;i++)
 	for(int j=1;j<=N;j++)
+		{
 		Matrix[i][j] = 0;
+		updateMap[i][j] = new bool[N+1];
+		for(int k=1;k<=N;k++)
+			updateMap[i][j][k] = false;
+		}
+return;
+}
+
+//Initialize the global matrix by memory allocation and assigning values
+void freeMatrix()
+{
+for(int i=1;i<=N;i++)
+        for(int j=1;j<=N;j++)
+                {
+                delete[] updateMap[i][j];
+                }
+
+for(int i=1;i<=N;i++)
+        {
+        delete[] Matrix[i];
+        delete[] updateMap[i];
+        }
+
+delete[] Matrix;
+delete[] updateMap;
+N = 0;
 return;
 }
 
 void updateMatrix(int i, int j, int k)
 	{
-	if( k <= (N-2) && (k<i ) && (i<N)&&(k<j))
+	updateCall++;
+	if( k <= (N-2) && (k<i ) && (i<N)&&(k<j) && !updateMap[i][j][k])
 		{
 
 		if(dbg) {
 			cout<<i<<" "<<j<<" "<<k<<endl;
 			}
 
-		updateCall++;
-		Matrix[j][i] = Matrix[j][i] - (Matrix[k][i]/Matrix[k][k])*Matrix[j][k];
-		updateCount++;
+		//Matrix[j][i] = Matrix[j][i] - (Matrix[k][i]/Matrix[k][k])*Matrix[j][k];
+		//if(false == updateMap[i][j])
+			{
+			Matrix[i][j] = Matrix[i][j] - (Matrix[i][k]*Matrix[k][j])/Matrix[k][k];
+			updateCount++;
+		/*	stringstream ss1; ss1<<k<<",";
+			updateMap[i][j]+=ss1.str(); */
+			updateMap[i][j][k] = true;
+			}
 		}
 	}
 
 void printMat()
 {
-for(int i=1;i<=N;i++,cout<<endl)
-	for(int j=1;j<=N;j++,cout<<' ')
-		cout<<setw(7)<<setprecision(4)<<Matrix[i][j];
+for(int i=1;i<=N;i++,cout<<endl/*printf("\n")*/)
+	for(int j=1;j<=N;j++)
+		cout<<setw(10)<<setprecision(2)<<fixed<<Matrix[i][j];//<<"{"<<updateMap[i][j]<<"}";
+		//printf("%5.2lf ",Matrix[i][j]);
 }
 
 // @Class subMat - This is basically the specification of a submatrix
@@ -262,13 +305,17 @@ return;
 int main()
 {
 int q;
-cout<<"Enter the size-parameter (N) for the matrix with size [ N X N ]"; cin>>q;
+//cout<<"Enter the size-parameter (N) for the matrix with size [ N X N ]"; 
+int count = 0;
+while(cin>>q /*&& count <1*/)
+	{
+count++;
 //assert(q&(q-1) == 0); //q has to be a non-negative integer
 
 N = q;  //(1<<q); 
 initializeMatrix(N);
 
-cout<<endl<<"Input matrix of size "<<N<<" X "<<N<<endl;
+//cout<<endl<<"Input matrix of size "<<N<<" X "<<N<<endl;
 
 for(int i=1;i<=N;i++)
 	for(int j=1;j<=N;j++)
@@ -276,16 +323,20 @@ for(int i=1;i<=N;i++)
 			cin>>Matrix[i][j];		
 		}
 
-cout<<" ========= Input Matrix ============== "<<endl;
-printMat();
-
+//cout<<" ========= Input Matrix ============== "<<endl;
+/*printMat();
+cout<<endl;
+*/
 subMat X(1,N,1,N), U(1,N,1,N), V(1,N,1,N), W(1,N,1,N); //All submatrices of N*N size
 
 funcA(X,U,V,W); 
-cout<<" Matrix was updated "<<updateCount<<" times out of "<<updateCall<<" calls...."<<endl;
+//cout<<" Matrix("<<N<<"X"<<N<<") was updated "<<updateCount<<" times out of "<<updateCall<<" calls...."<<endl;
 
-cout<<" ======== Output Matrix ============= "<<endl;
 printMat();
+cout<<endl;
+
+freeMatrix();
+	}
 
 return 0;
 }
---
+++ GaussianElimination/serialCode/serialGaussian.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+#include<iomanip>
+#include<cassert>
+using namespace std;
+
+double ** Matrix = NULL; // Bad code!! Need to add a class
+int ** updateMap = NULL;
+int N;
+static int updateCount = 0, updateCall = 0;
+bool dbg = false;//true;
+
+//Initialize the global matrix by memory allocation and assigning values
+void initializeMatrix(int N)
+{
+Matrix = new double*[N+1];
+updateMap = new int*[N+1];
+
+for(int i=1;i<=N;i++)
+	{
+	Matrix[i] = new double[N+1];
+	updateMap[i] = new int[N+1];
+	}
+
+for(int i=1;i<=N;i++)
+	for(int j=1;j<=N;j++)
+		{
+		Matrix[i][j] = 0;
+		updateMap[i][j] = 0;
+		}
+return;
+}
+
+void updateMatrix(int i, int j, int k)
+	{
+	updateCall++;
+	if( k <= (N-2) && (k<i) && (i<N)&&(k<j)&&(j<=N)&&(k>=1))
+		{
+
+		if(dbg) {
+			cout<<i<<" "<<j<<" "<<k<<endl;
+			}
+
+		//Matrix[j][i] = Matrix[j][i] - (Matrix[k][i]/Matrix[k][k])*Matrix[j][k];
+		//if(false == updateMap[i][j])
+			{
+			Matrix[i][j] = Matrix[i][j] - (Matrix[i][k]/Matrix[k][k])*Matrix[k][j];
+			updateCount++;
+			updateMap[i][j]++;
+			}
+		}
+	}
+
+//Initialize the global matrix by memory allocation and assigning values
+void freeMatrix()
+{
+
+for(int i=1;i<=N;i++)
+        {
+        delete[] Matrix[i];
+        delete[] updateMap[i];
+        }
+
+delete[] Matrix;
+delete[] updateMap;
+N = 0;
+return;
+}
+
+
+void printMat()
+{
+for(int i=1;i<=N;i++,cout<<endl)
+	for(int j=1;j<=N;j++)
+		cout<<setw(10)<<setprecision(2)<<fixed<<Matrix[i][j];//<<"("<<updateMap[i][j]<<")";
+}
+
+int main()
+{
+int q;
+//cout<<"Enter the size-parameter (N) for the matrix with size [ N X N ]"; 
+while(cin>>q)
+	{
+//assert(q&(q-1) == 0); //q has to be a non-negative integer
+
+N = q;  //(1<<q); 
+initializeMatrix(N);
+
+//cout<<endl<<"Input matrix of size "<<N<<" X "<<N<<endl;
+
+for(int i=1;i<=N;i++)
+	for(int j=1;j<=N;j++)
+		{
+			cin>>Matrix[i][j];		
+		}
+
+//cout<<" ========= Input Matrix ============== "<<endl;
+//printMat();
+
+for(int k=1;k<(N-1);k++)
+	for(int i=k+1;i<N;i++)
+		for(int j=k+1;j<=N;j++)
+			updateMatrix(i,j,k);	 
+
+//cout<<" Matrix was updated "<<updateCount<<" times out of "<<updateCall<<" calls...."<<endl;
+
+//cout<<" ======== Output Matrix ============= "<<endl;
+printMat();
+cout<<endl;
+
+freeMatrix();
+
+	}
+
+return 0;
+}
